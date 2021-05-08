09d64aa44e499541b8199be80fa0dbba5ac7434c
Ulyana Klyuchkovska <ukl@ciklum.com>
2021-05-08 23:28:48 +0300

    json place holder api

---
+++ app/src/main/java/ru/startandroid/develop/retrofittask/JsonPlaceHolderApi.kt
@@ -0,0 +1,9 @@
+package ru.startandroid.develop.retrofittask
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface JsonPlaceHolderApi {
+    @get:GET("posts")
+    val messages: Call<List<MessageApiModel?>?>?
+}
