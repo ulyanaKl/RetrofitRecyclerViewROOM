ad3bc42b71f6b4b3db554b794856665db7de7fb3
Ulyana Klyuchkovska <ukl@ciklum.com>
2021-05-09 23:59:01 +0300

    database

---
+++ app/src/main/java/ru/startandroid/develop/retrofittask/MessageDatabase.kt
@@ -0,0 +1,26 @@
+package ru.startandroid.develop.retrofittask
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [MessageEntity::class], version = 3, exportSchema = false)
+abstract class MessageDatabase : RoomDatabase() {
+    abstract val messageDao: MessageDao?
+
+    companion object {
+        private var messageDatabase: MessageDatabase? = null
+        @JvmStatic
+        fun getMessageDatabase(context: Context): MessageDatabase? {
+            if (messageDatabase == null) {
+                messageDatabase = Room.databaseBuilder(context.applicationContext,
+                        MessageDatabase::class.java, "database-name")
+                        .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
+                        .build()
+            }
+            return messageDatabase
+        }
+    }
+}
